39b71e56191ed91f99726b1efe45529ef955d871
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-18 07:49:58 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, age) VALUES ("Sam", 1), ("Adrienne", 2), ("Pat", 3), ("Cortney", 4),
+("Rachel", 5), ("Tim", 6), ("Elle", 7), ("Jamie", 8), ("Marlena", 9), ("Terri", 10),
+("Ali", 11), ("Jessica", 12), ("Ryan", 13), ("Alexis", 14), ("Paula", 15),
+("Devin", 16), ("Claire", 17), ("Jenny", 18), ("Julia", 19), ("Alice", 20)
