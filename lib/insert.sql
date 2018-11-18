357fd80e1f43282f0422c788a1683c86913e4082
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-18 07:59:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,15 @@ INSERT INTO users (name, age) VALUES ("Sam", 1), ("Adrienne", 2), ("Pat", 3), ("
 ("Rachel", 5), ("Tim", 6), ("Elle", 7), ("Jamie", 8), ("Marlena", 9), ("Terri", 10),
 ("Ali", 11), ("Jessica", 12), ("Ryan", 13), ("Alexis", 14), ("Paula", 15),
 ("Devin", 16), ("Claire", 17), ("Jenny", 18), ("Julia", 19), ("Alice", 20)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("1 - Help me with a thing", "charity", 5000, "start date", "end date"),
+("2 - I'm not that creative", "charity", 2000, "start date", "end date"),
+("3 - But I do like..", "product", 10000, "start date", "end date"),
+("4 - To get practice writing..", "product", 8000, "start date", "end date"),
+("5 - My own tables..", "charity", 6000, "start date", "end date"),
+("6 - And really..", "charity", 15000, "start date", "end date"),
+("7 - My own code in general", "product", 500, "start date", "end date"),
+("8 - I like to get a feel for..", "product", 4000, "start date", "end date"),
+("9 - The pattern/routine..", "charity", 7000, "start date", "end date"),
+("10 - So hopefully it becomes second nature", "charity", 800, "start date", "end date");
