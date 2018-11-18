039a058108923446ddcee247549e18080d56c809
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-18 08:00:09 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (name, age) VALUES ("Sam", 1), ("Adrienne", 2), ("Pat", 3), ("Cortney", 4),
 ("Rachel", 5), ("Tim", 6), ("Elle", 7), ("Jamie", 8), ("Marlena", 9), ("Terri", 10),
 ("Ali", 11), ("Jessica", 12), ("Ryan", 13), ("Alexis", 14), ("Paula", 15),
-("Devin", 16), ("Claire", 17), ("Jenny", 18), ("Julia", 19), ("Alice", 20)
+("Devin", 16), ("Claire", 17), ("Jenny", 18), ("Julia", 19), ("Alice", 20);
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("1 - Help me with a thing", "charity", 5000, "start date", "end date"),
